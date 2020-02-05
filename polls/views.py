0dd59ecc2c28db90fe5f6f101f8dee33a60eddc5
brookepaxman <brooke.paxman@utexas.edu>
2020-02-05 13:36:10 -0600

    new users

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published users."""
-        return User.objects.order_by('-pub_date')[:5]
+        return User.objects.order_by('-pub_date')[:10]
 
 
 class DetailView(generic.DetailView):
