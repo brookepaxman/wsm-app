b0916a754b68823890fd01f46e9338bc349b560c
brookepaxman <brooke.paxman@utexas.edu>
2020-03-23 16:35:55 -0500

    no message

--- polls/urls.py
+++ polls/urls.py
@@ -10,8 +10,8 @@ router.register(r'dummys', views.DummyView)
 
 app_name = 'polls'
 urlpatterns = [
-    path('', include(router.urls)),
     path('', views.IndexView.as_view(), name='index'),
+    path('', include(router.urls)),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('line-chart/', views.ChartView.as_view(), name='line-chart'),
     #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
