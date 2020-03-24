463ba3d96a112fe59f8f94bb25e833b6429a9b15
kaylantran <kaylatran@utexas.edu>
2020-03-23 20:39:00 -0500

    added analysis urls

--- polls/urls.py
+++ polls/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('line-chart/', views.ChartView.as_view(), name='line-chart'),
+    path('analysis/', views.MultiView.as_view(), name='analysis'),
+    path('analysis/user_input/', views.UserInputView.as_view(),name='user_input'),
     #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     #path('<int:user_id>/vote/', views.vote, name='vote'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
