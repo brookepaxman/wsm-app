68aa73665ba1072a714f5c75b7a8080d512ea238
kaylantran <kaylatran@utexas.edu>
2020-03-23 20:41:34 -0500

    create forms.py

---
+++ polls/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class sleepQualityForm(forms.Form):
+    sleepQuality = forms.IntegerField(min_value=1,max_value=5)
+    sleepDisruptions = forms.CharField(widget=forms.Textarea)
+    sleepNotes = forms.CharField(widget=forms.Textarea)
