f0361df0a75b0796d86b36a56f3cd63bdb0ff24a
Kayla Tran <kaylatran@MacBooks-MacBook-Pro-3.local>
2020-04-12 13:45:24 -0500

    updated user_input

--- polls/forms.py
+++ polls/forms.py
@@ -8,9 +8,9 @@ from django.contrib.admin.widgets import AdminDateWidget
 class sleepQualityForm(forms.Form):
     sleepQuality = forms.IntegerField(label="Rate your sleep from 1-5",min_value=1,max_value=5)
     numDisruptions = forms.IntegerField(label="Number of sleep disruptions",min_value = 0)
-    # sleepNotes = forms.CharField(label="Enter notes about your sleep:",widget=forms.Textarea)
+    sleepNotes = forms.CharField(label="Enter notes about your sleep:",widget=forms.Textarea)
     # inputDate = forms.DateField(widget=forms.SelectDateWidget(years=[1990,1991,1992]))
-    sleepNotes = forms.CharField(widget=forms.TextInput(attrs={'size': 50}))
+    # sleepNotes = forms.CharField(widget=forms.TextInput(attrs={'size': 50}))
 
 
 class calendarForm(forms.Form):
