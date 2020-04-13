1bbbf6efe9a33f8cf5b824faab9a0efba5644354
Nithanth <nithanth.ram@utexas.edu>
2020-04-13 08:54:48 -0500

    about page linked

--- polls/views.py
+++ polls/views.py
@@ -94,8 +94,16 @@ class GenerateView(generic.FormView):
                 args = {'form':form, 'test':test}
         return render(request, self.template_name, args)
 
+
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
+
+
+class AboutView(generic.ListView):
+    template_name = 'polls/about.html'
+
+    def get(self,request):
+        return render(request, self.template_name)
 
 class StatView(viewsets.ModelViewSet):
     def get_queryset(self):         # this returns most recent session of the user that is logged in
